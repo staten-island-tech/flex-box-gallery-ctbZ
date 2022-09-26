2d566c2ad9efee8a0f644406e330b756fc71d115
ctbZ <113132299+ctbZ@users.noreply.github.com>
2022-09-26 12:28:15 -0400

    js animation

---
+++ app.js
@@ -0,0 +1,38 @@
+window.addEventListener('DOMContentLoaded', setup)
+
+function setup() {
+    const options= {
+        rootMargin: '0%'
+    }
+
+    const observer = new IntersectionObserver((entries,observer) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting) {
+                entry.target.classList.add('show');
+                oberserver.unobserve(entry.target)
+            } else {
+                return;
+            }
+        })
+    }, options);
+
+    const h1 = document.querySelector('h1');
+    observer.observe(h1);
+}
+
+
+
+
+
+// const observer = new IntersectionObserver(function (entries) {
+//     entries.forEach((entry) => {
+//         console.log(entry)
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('show')
+//         }
+//     });
+// });
+
+
+// const hiddenElements = document.querySelectorAll('hidden');
+// observer.observe(document.querySelector(Element));
